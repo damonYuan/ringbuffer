6f8a01c2ee0ed4387382d3ec569fff3c4b48e2e8
damon.yuan <damon.yuan@okg.com>
2024-01-18 20:14:06 +0800

    main: update

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ README
 
 1. [Implementing a Ring Buffer in Java](https://www.baeldung.com/java-ring-buffer)
 2. [Lock Free Ring Buffer](https://www.lenshood.dev/2021/04/19/lock-free-ring-buffer/)
+3. [disruptor](https://github.com/LMAX-Exchange/disruptor/tree/master)
+4. [hikari](https://github.com/brettwooldridge/HikariCP)
+   1. [读懂 HikariCP](https://mp.weixin.qq.com/s/ayCXva50GRsV8_X5aIykRQ)
+   2. [全面讲解 HikariCP](https://www.cnblogs.com/ZhangZiSheng001/p/12329937.html)
+5. [Using JDK 9 Memory Order Modes](https://gee.cs.oswego.edu/dl/html/j9mm.html#summarysec)
+   1. [梳理 Varhandle](https://zhuanlan.zhihu.com/p/145654924)
---
+++ src/main/java/com/damonyuan/test/RingBuffer.java
@@ -0,0 +1,61 @@
+package com.damonyuan.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RingBuffer<E> implements IRingBuffer<E> {
+    private static final int DEFAULT_CAPACITY = 8;
+    private final int capacity;
+    private final int mask;
+    private final E[] data;
+    private AtomicInteger head; // next read
+    private AtomicInteger tail; // next write
+
+    public RingBuffer(final int capacity) {
+        this.capacity = (capacity < 1) ? DEFAULT_CAPACITY : findPowerOfTwo(capacity);
+        this.mask = capacity - 1;
+        this.data = (E[]) new Object[capacity];
+        this.head = new AtomicInteger(0);
+        this.tail = new AtomicInteger(0);
+    }
+
+    private int findPowerOfTwo(final int capacity) {
+        int result = capacity - 1;
+        result |= result >> 1;
+        result |= result >> 2;
+        result |= result >> 4;
+        result |= result >> 8;
+        result |= result >> 16;
+        result++;
+        return result;
+    }
+
+    @Override
+    public boolean offer(final E element) {
+        return false;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
